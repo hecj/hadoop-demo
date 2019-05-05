4b0f0cb505fe6943075c2b0ab5e405a87ef1bdd4
何超杰 <hechaojie@xylink.cn>
2019-05-06 00:36:45 +0800

    自定义数据分发规则

--- src/main/java/com/hadoop/mr/flow/JobSubmitter.java
+++ src/main/java/com/hadoop/mr/flow/JobSubmitter.java
@@ -17,17 +17,20 @@ public class JobSubmitter {
 		job.setJarByClass(JobSubmitter.class);
 		job.setMapperClass(FlowCountMapper.class);
 		job.setReducerClass(FlowCountReduce.class);
+
+		// 设置参数：maptask在做数据分区时，用哪个分区逻辑类  （如果不指定，它会用默认的HashPartitioner）
+		job.setPartitionerClass(ProvincePartitioner.class);
+		// 由于我们的ProvincePartitioner可能会产生6种分区号，所以，需要有6个reduce task来接收
+		job.setNumReduceTasks(6);
 		
 		job.setMapOutputKeyClass(Text.class);
 		job.setMapOutputValueClass(FlowBean.class);
-		
+
 		job.setOutputKeyClass(Text.class);
 		job.setOutputValueClass(FlowBean.class);
 		
 		FileInputFormat.setInputPaths(job,new Path("/hadoop/flow_in"));
 		FileOutputFormat.setOutputPath(job,new Path("/hadoop/flow_out"));
-		
-		job.setNumReduceTasks(1);
 		
 		job.waitForCompletion(true);
 		
